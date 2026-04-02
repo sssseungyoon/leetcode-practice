d0db2bd3a6c3c22221b0c13bea3428700363edec
sssseungyoon <140358317+sssseungyoon@users.noreply.github.com>
2026-04-02 14:27:03 +0100

    now this logic works

--- exercise3381.py
+++ exercise3381.py
@@ -4,23 +4,24 @@ from typing import List
 class Solution:
     # this will have O(n x k) time complexity
     def maxSubarraySum(self, nums: List[int], k: int) -> int:
-        tracking: dict[int, tuple[int, bool]] = {
-            i: (-(10**100), True) for i in range(k)
-        }
-        tracking[0] = (sum(nums[:k]), True)
+        tracking: dict[int, tuple[int, int]] = {i: (-(10**100), 0) for i in range(k)}
+        # tuple (maximum, tracking, canMerge)
+        tracking[0] = (sum(nums[:k]), sum(nums[:k]) if sum(nums[:k]) >= 0 else 0)
         print(tracking)
 
         for i in range(k, len(nums)):
             index = (i + 1) % k
             new_chunk = sum(nums[(i + 1 - k) : (i + 1)])
-            existing_chunk = tracking[index][0]
+            existing_chunk = tracking[index][1]
+            merged_chunk = new_chunk + existing_chunk
             print("new:", new_chunk, "existing:", existing_chunk, "i%k:", index)
-            canMerge = tracking[index][1]
-            if canMerge:
-                localMax = max(existing_chunk, new_chunk, existing_chunk + new_chunk)
-            else:
-                localMax = max(existing_chunk, new_chunk)
-            tracking[index] = (localMax, (localMax != existing_chunk))
+            # case when the existing chunk is greater than the current max
+            max_val = tracking[index][0]
+            if merged_chunk > max_val:
+                max_val = merged_chunk
+            # merge when the subtotal is greater than 0 else reset
+            merged_chunk = merged_chunk if merged_chunk >= 0 else 0
+            tracking[index] = (max_val, merged_chunk)
             print("i:", i, "tracking:", tracking)
 
         output = -(10**100)
@@ -36,6 +37,6 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [-5, 1, 2, -3, 4]
-    k = 2
+    nums = [-10, -1]
+    k = 1
     print(sol.maxSubarraySum(nums, k))
